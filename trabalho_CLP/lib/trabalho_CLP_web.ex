3dede120766e9870a8eb10494cad24bdb03548da
patryckro <patryckmoulaz@gmail.com>
2021-05-02 17:27:09 -0300

    Live resurt on

--- trabalho_CLP/lib/trabalho_CLP_web.ex
+++ trabalho_CLP/lib/trabalho_CLP_web.ex
@@ -83,6 +83,7 @@ defmodule Trabalho_CLPWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import Trabalho_CLPWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
